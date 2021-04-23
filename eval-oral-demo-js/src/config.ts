1a701e8bbdee66aa8c14d1a05008bcad10e23962
zoupeng <zoupeng@unisound.com>
2021-04-23 12:51:10 +0800

    update

--- eval-oral-demo-js/src/config.ts
+++ eval-oral-demo-js/src/config.ts
@@ -4,11 +4,11 @@ export enum AiCode {
 
 export const Config = {
   [AiCode.Eval]: {
-    appKey: 'appKey',
-    secret: 'secret',
+    appKey: 'uus46rhwq3x75562p2cd7f7qplwso6wt5xd4qqae',
+    secret: '41834fffb1dbc925eddba3dca1e6f036',
     path: {
-      cn: 'servicePath',
-      en: 'servicePath',
+      cn: 'wss://wss-edu.hivoice.cn:443/ws/eval/',
+      en: 'wss://wss-edu.hivoice.cn:443/ws/eval/',
     },
   },
 };
